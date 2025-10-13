ff71572ae6f4f6786956ae7c05b3f91fbce51d7b
joaopcipriani <67130891+joaopcipriani@users.noreply.github.com>
2025-10-13 18:03:37 -0300

    NOVO

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -1,27 +1,14 @@
 from minidump.minidumpfile import MinidumpFile
 import os
-from pdbparse.pe import PE
-from pdbparse import pdb
-import glob
-
-SYMBOLS_PATH = "symbols"
-
-def load_symbols():
-    symbols = {}
-    for pdb_file in glob.glob(os.path.join(SYMBOLS_PATH, "*.pdb")):
-        try:
-            pdb_data = pdb.PDB(open(pdb_file, "rb"))
-            symbols[pdb_file] = pdb_data
-        except Exception:
-            pass
-    return symbols
 
 def analyze_dump(dmp_bytes):
     os.makedirs("uploads", exist_ok=True)
-    with open("uploads/temp.dmp", "wb") as f:
+    dump_path = "uploads/temp.dmp"
+
+    with open(dump_path, "wb") as f:
         f.write(dmp_bytes)
 
-    dump = MinidumpFile.parse("uploads/temp.dmp")
+    dump = MinidumpFile.parse(dump_path)
     results = []
     for thread in dump.threads:
         stack = [hex(frame.instruction) for frame in thread.stack]
